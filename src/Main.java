fd3466a5da90b280661af9dd76d01afc219eddeb
Dimon <Dimon.1322@yandex.ru>
2026-02-13 05:50:05 +0300

    Cycles: while and do-whiel again update in №5

--- src/Main.java
+++ src/Main.java
@@ -93,24 +93,25 @@ public class Main {
         }
 
         System.out.println("№5");
-        byte charge = 20;
+        byte charge = 0;
         short minute = 0;
         byte overheats = 0;
         while (charge < 100 && overheats <= 3) {
-//            System.out.println(minute + " minute | заряд " + charge);
+            System.out.println(minute + " minute | заряд " + charge);
             minute++;
-            if (minute % 10 == 0) {
+            if ((minute - overheats) % 10 == 0) {
                 overheats++;
-                charge = (byte) (charge + 2);
+                minute++;
                 if (overheats >= 3) {
                     System.out.println("Зарядка прекращена. Текущий заряд: " + charge + " %");
+                    minute = (short) (minute - 2);
                     break;
                 }
-//                System.out.println("перегрев");
+                System.out.println("перегрев");
                 continue;
             }
             charge = (byte) (charge + 2);
         }
-        System.out.println("Зарядка заняла " + (minute + overheats * 2) + " минут");
+        System.out.println("Зарядка заняла " + minute + " минут");
     }
 }
