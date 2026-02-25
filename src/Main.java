5ade5303afcffdade2ee5eb2c51c0ebf8724cd13
Dimon <Dimon.1322@yandex.ru>
2026-02-25 07:09:13 +0300

    methods correction

--- src/Main.java
+++ src/Main.java
@@ -2,50 +2,48 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     private static double operSystem;
-
     // #1
-public static int calculatingLeapYear(int leapYear){
-    if (leapYear < 1584) {
-        System.out.println(leapYear + " год не является високосным, т.к. в те времена данное понятие ещё не было открыто");
-    } else if (leapYear % 4 == 0 && leapYear % 100 != 0 || leapYear % 400 == 0) {
-        System.out.println(leapYear + " год — високосный год");
-    } else {
-        System.out.println(leapYear + " год — невисокосный год");
+    public static int calculatingLeapYear(int leapYear) {
+        if (leapYear < 1584) {
+            System.out.println(leapYear + " год не является високосным, т.к. в те времена данное понятие ещё не было открыто");
+        } else if (leapYear % 4 == 0 && leapYear % 100 != 0 || leapYear % 400 == 0) {
+            System.out.println(leapYear + " год — високосный год");
+        } else {
+            System.out.println(leapYear + " год — невисокосный год");
+        }
+        return leapYear;
     }
-    return leapYear;
-}
-//#2
-public static int definingVersion(int operSystem,int deviceYear ){
-    if (deviceYear < 2015 && operSystem == 0) {
-        System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-    } else if (deviceYear < 2015 && operSystem == 1) {
-        System.out.println("Установите облегченную версию приложения для Android по ссылке");
-    } else if (deviceYear >= 2015 && operSystem == 0) {
-        System.out.println("Установите версию приложения для iOS по ссылке");
-    } else if (deviceYear >= 2015 && operSystem == 1) {
-        System.out.println("Установите версию приложения для Android по ссылке");
-    } else {
-        System.out.println("Операционная система неопознанна");
+    //#2
+    public static int definingVersion(int operSystem, int deviceYear) {
+        if (deviceYear < 2015 && operSystem == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (deviceYear < 2015 && operSystem == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (deviceYear >= 2015 && operSystem == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (deviceYear >= 2015 && operSystem == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Операционная система неопознанна");
+        }
+        return operSystem;
     }
-
-    return operSystem;
-}
-//#3
-public static int determiningDeliveryDistance(int distance) {
-    if (distance <= 20) {
-        byte timesDelivery = 1;
-        System.out.println("Потребуется дней " + timesDelivery);
-    } else if (distance <= 60) {
-        byte timesDelivery = 2;
-        System.out.println("Потребуется дней " + timesDelivery);
-    } else if (distance <= 100) {
-        byte timesDelivery = 3;
-        System.out.println("Потребуется дней " + timesDelivery);
-    } else {
-        System.out.println("Доставки нет");
+    //#3
+    public static int determiningDeliveryDistance(int distance) {
+        if (distance <= 20) {
+            byte timesDelivery = 1;
+            System.out.println("Потребуется дней " + timesDelivery);
+        } else if (distance <= 60) {
+            byte timesDelivery = 2;
+            System.out.println("Потребуется дней " + timesDelivery);
+        } else if (distance <= 100) {
+            byte timesDelivery = 3;
+            System.out.println("Потребуется дней " + timesDelivery);
+        } else {
+            System.out.println("Доставки нет");
+        }
+        return distance;
     }
-    return distance;
-}
     public static void main(String[] args) {
 
         System.out.println("#1");
@@ -55,11 +53,10 @@ public static int determiningDeliveryDistance(int distance) {
         System.out.println("#2");
         byte clientOS = 1;
         short clientDeviceYear = 2005;
-        definingVersion(clientOS,clientDeviceYear);
+        definingVersion(clientOS, clientDeviceYear);
 
         System.out.println("#3");
         short deliveryDistance = 95;
         determiningDeliveryDistance(deliveryDistance);
-
     }
 }
